4949456381f324cbd70dc05f4953af8887b36bae
ETHNOTECH <ETHNOTECH@DESKTOP-QEBAMDG>
2025-07-20 23:55:41 -0700

    First java project

---
+++ C2TC_Java_Sharan/src/day1/FirstProgram.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the java Class");
+		// TODO Auto-generated method stub
+
+	}
+
+}
